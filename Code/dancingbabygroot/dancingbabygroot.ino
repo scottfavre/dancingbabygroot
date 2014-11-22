08f941ac8da55fe11d2196bcf44160cb7f0e08b0
Scott Favre <sfavre@neweagle.net>
2014-11-22 12:05:04 -0500

    mp3 playing works

---
+++ Code/dancingbabygroot/dancingbabygroot.ino
@@ -0,0 +1,114 @@
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+      // create breakout-example object!
+      //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+      // create shield-example object!
+      Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+      
+      
+File currentFile;
+      
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit VS1053 Library Test");
+
+  // initialise the music player
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial.println(F("VS1053 found"));
+
+  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+ 
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+    
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(50,50);
+
+  /***** Two interrupt options! *******/ 
+  // This option uses timer0, this means timer1 & t2 are not required
+  // (so you can use 'em for Servos, etc) BUT millis() can lose time
+  // since we're hitchhiking on top of the millis() tracker
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
+  
+  // This option uses a pin interrupt. No timers required! But DREQ
+  // must be on an interrupt pin. For Uno/Duemilanove/Diecimilla
+  // that's Digital #2 or #3
+  // See http://arduino.cc/en/Reference/attachInterrupt for other pins
+  // *** This method is preferred
+  if (! musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT))
+    Serial.println(F("DREQ pin is not an interrupt pin"));
+    
+  currentFile = SD.open("/").openNextFile();
+}
+
+void loop() {  
+  if(!musicPlayer.playingMusic) {
+    currentFile.close();
+    currentFile = currentFile.openNextFile();
+    
+    if(!currentFile) {
+      Serial.println("Done playing music");
+      currentFile = SD.open("/").openNextFile();
+    }
+    
+    if(!musicPlayer.startPlayingFile(currentFile.name())) {
+      Serial.println("Could not open file:");
+      Serial.println(currentFile.name());
+      while (1);
+    } else {
+      Serial.println("Playing:");
+      Serial.println(currentFile.name());
+    }
+  }
+  else
+  {
+    delay(10);
+  }
+}
+
+
+/// File listing helper
+//void printDirectory(File dir, int numTabs) {
+//   while(true) {
+//     
+//     File entry =  dir.openNextFile();
+//     if (! entry) {
+//       // no more files
+//       //Serial.println("**nomorefiles**");
+//       break;
+//     }
+//     for (uint8_t i=0; i<numTabs; i++) {
+//       Serial.print('\t');
+//     }
+//     Serial.print(entry.name());
+//     if (entry.isDirectory()) {
+//       Serial.println("/");
+//       printDirectory(entry, numTabs+1);
+//     } else {
+//       // files have sizes, directories do not
+//       Serial.print("\t\t");
+//       Serial.println(entry.size(), DEC);
+//     }
+//     entry.close();
+//   }
+//}
+
